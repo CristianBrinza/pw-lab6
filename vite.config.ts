649b06a2bc029423d333702ecedccb742fcbc9d7
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-04-29 12:56:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  // server: {
+  //   port: 3001, // Change the port to 3000
+  // },
+  base: 'https://cristianbrinza.github.io/pw-lab6/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'], // Optional: If you have global test setup file
+  },
 });
